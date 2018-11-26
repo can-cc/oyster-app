e51ce576518be2843f524361b4f706a4d6474b42
fwchen <me@chenfangwei.com>
2018-11-26 21:54:11 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -42,17 +42,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Future<List<int>> fakeRequest(int from, int to) async {
+  return Future.delayed(Duration(seconds: 2), () {
+    return List.generate(to - from, (i) => i + from);
+  });
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -61,78 +58,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Future<Atoms> atoms;
+  List<Atom> items = List();
+  ScrollController _scrollController = new ScrollController();
+  bool isPerformingRequest = false;
 
   @override
   void initState() {
+    debugPrint('debug init');
     super.initState();
-    debugPrint('hihihihi->');
-    atoms = fetchAtoms();
-  }
-
-
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    _getMoreData();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        _getMoreData();
+      }
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    debugPrint('hi000');
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  _getMoreData() async {
+    debugPrint('Hihihi->');
+    if (!isPerformingRequest) {
+      setState(() => isPerformingRequest = true);
+      List<Atom> newEntries = (await fetchAtoms()).items; //returns empty list
+      if (newEntries.isEmpty) {
+        double edge = 50.0;
+        double offsetFromBottom = _scrollController.position.maxScrollExtent - _scrollController.position.pixels;
+        if (offsetFromBottom < edge) {
+          _scrollController.animateTo(
+              _scrollController.offset - (edge -offsetFromBottom),
+              duration: new Duration(milliseconds: 500),
+              curve: Curves.easeOut);
+        }
+      }
+      setState(() {
+        items.addAll(newEntries);
+        isPerformingRequest = false;
+      });
+    }
+  }
+
+  Widget _buildProgressIndicator() {
+    return new Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Opacity(
+          opacity: isPerformingRequest ? 1.0 : 0.0,
+          child: new CircularProgressIndicator(),
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Infinite ListView"),
+      ),
+      body: ListView.builder(
+        itemCount: items.length + 1,
+        itemBuilder: (context, index) {
+          if (index == items.length) {
+            return _buildProgressIndicator();
+          } else {
+            return ListTile(title: new Text(items[index].title));
+          }
+        },
+        controller: _scrollController,
+      ),
     );
   }
 }
