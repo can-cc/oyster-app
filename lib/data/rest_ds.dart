9dfaccd29e94b31a97583d5cc0344568180a3332
fwchen <me@chenfangwei.com>
2018-12-20 22:16:52 +0800

    format dart code

--- lib/data/rest_ds.dart
+++ lib/data/rest_ds.dart
@@ -1,13 +1,11 @@
 import 'package:osyter_app/utils/network_util.dart';
 import 'package:osyter_app/model/Feed.dart';
 
-
 final SERVER_HOST = "http://192.168.50.77:7788";
 
 class RestDataSource implements AuthStateListener {
   NetworkUtil _netUtil = new NetworkUtil();
 
-  static final BASE_URL = "http://192.168.50.77:7788";
   static final LOGIN_URL = BASE_URL + "/api/login";
 
   Future<ApiResult> login(String username, String password) {
