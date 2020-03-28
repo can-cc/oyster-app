717778c02f976d4b22571b3a2ea3f384a67804fe
fwchen <me@chenfangwei.com>
2020-03-28 20:24:08 +0800

    update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,17 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/chenfangwei/lib/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/chenfangwei/workspace/oyster-app"
+<<<<<<< HEAD
 export "FLUTTER_TARGET=lib/main.dart"
+=======
+export "FLUTTER_TARGET=/Users/chenfangwei/workspace/oyster-app/lib/main.dart"
+>>>>>>> update
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/chenfangwei/lib/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+<<<<<<< HEAD
+=======
+export "TRACK_WIDGET_CREATION=true"
+>>>>>>> update
